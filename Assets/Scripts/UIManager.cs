841118283d40a6bccc1714b307a9387adbcb5941
Juan Carlos Cardoso <papo@nati.games>
2020-01-29 07:49:26 -0600

    Initial Commit

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+     public Text feedback;
+
+    public void setFeedback(string status)
+    {
+        feedback.text = status;
+    }
+}
---
+++ Assets/Scripts/user.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+[Serializable]
+public class user
+{
+    public bool success;
+    public string error;
+    public int userid;
+    public string email;
+}
